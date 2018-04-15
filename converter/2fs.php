3b78284b9806ad0006a7d290d392cb93189341ae
Herb Miller <herb@bobbingwide.com>
2018-04-15 19:35:09 +0100

    Issue #25 - two file scan to determine which hooks have changed

---
+++ converter/2fs.php
@@ -0,0 +1,210 @@
+<?php 
+/* Two file scan 
+   Compare g813.names vs c813.names
+	 documenting changes
+	 Where c813.names is the master and we're looking for changes 
+	 
+	 
+c813.names extract
+		
+[hook _admin_menu action 0 1 1]
+[hook add_admin_bar_menus action 0 1 0]
+[hook add_menu_classes filter 1 1 0]
+[hook add_meta_boxes action 2 1 2]
+[hook add_meta_boxes_post action 1 1 0]
+[hook admin_action_edit action 0 1 0]
+[hook admin_bar_init action 0 1 0]
+[hook admin_bar_menu action 1 1 16]
+
+[hook admin_body_class filter 1 1 1]
+[hook admin_enqueue_scripts action 1 1 11]
+
+	
+g813.names extract
+ 
+[hook _admin_menu action 0 1 1]
+[hook add_admin_bar_menus action 0 1 0]
+[hook add_menu_classes filter 1 1 0]
+[hook add_meta_boxes action 2 1 2]
+[hook add_meta_boxes_post action 1 1 0]
+[hook admin_action_edit action 0 1 0]
+[hook admin_bar_init action 0 1 0]
+[hook admin_bar_menu action 1 1 16]
+
+[hook admin_body_class filter 1 1 0]
+[hook admin_enqueue_scripts action 1 1 8]
+*/
+
+$scanner = new two_file_scan();
+$scanner->scan();
+$scanner->summary();
+
+class two_file_scan {
+
+	public $added = 0;
+	public $changed = 0;
+	public $deleted = 0;
+	public $same = 0;
+
+	function __construct() {  
+
+		$this->g813names = file( "../play/g813.names" );
+		$this->c813names = file( "../play/c813.names" );
+		$this->gc = count( $this->g813names );
+		$this->cc = count( $this->c813names );
+	
+		echo "G " . $this->gc . PHP_EOL;
+		echo "C " . $this->cc . PHP_EOL;
+		
+		$this->added = 0;
+		$this->changed = 0;
+		$this->deleted = 0;
+		$this->same = 0;
+	}
+	
+	function summary() {
+	  $this->oline( "Total Added" , $this->added );
+		$this->oline( "Total Changed" , $this->changed );
+		$this->oline( "Total Deleted" , $this->deleted );
+		$this->oline( "Total Same" , $this->same );
+	}
+	
+	function oline( $text, $value ) {
+		echo $text . ',' . $value . PHP_EOL;
+	}
+	
+	
+	function scan() {
+		$gi = 0;
+		$this->gline = $this->g813names[ $gi ]; 
+	
+		//echo $gline;
+	
+		$ci = 0;
+		$this->cline = $this->c813names[ $ci ];
+		//echo $cline;
+	
+		
+		while ( ( ( $ci + 1 ) < $this->cc ) || ( ( $gi + 1 ) < $this->gc ) ) {
+			$this->gline = $this->g813names[ $gi ];
+			$this->cline = $this->c813names[ $ci ];
+			$this->gparts = $this->get_parts( $this->gline ); 
+			$this->ghook = $this->get_hook( $this->gparts );
+			$this->cparts = $this->get_parts( $this->cline );
+			$this->chook = $this->get_hook( $this->cparts );
+		
+			if ( $this->ghook == $this->chook ) {
+				$this->compare_matches( $this->gparts, $this->cparts );
+				$gi = $this->nexti( $gi, $this->gc );
+				$ci = $this->nexti( $ci, $this->cc );
+			} elseif ( $this->ghook > $this->chook ) {
+			 
+				$this->deleted_classic( $this->cparts );
+				$ci = $this->nexti( $ci, $this->cc );
+			   
+			} else { /* ghook < chook - new Gutenberg hook */
+				$this->new_hook( $this->gparts );
+				$gi = $this->nexti( $gi, $this->gc );
+			}
+			echo PHP_EOL;
+		}
+		
+		
+		
+			 
+	
+	}
+	
+	/**
+	 * Sets the next index
+	 * 
+	 * Increments the index until the end is reached.
+	 * @param integer $index the array index
+	 * @param integer $count the number of items in the array
+	 * @return integer next index
+	 */ 
+	function nexti( $index, $count ) {
+		if ( ( $index + 1 ) < $count ) {
+			$index++;
+		}
+		//echo $index;
+		return $index;
+	}
+	/**
+	 * Converts line into an array of parts for each "hook" shortcode
+	
+	 * `[hook $hook $type $num_args $count $attached]`
+	 */
+	
+	function get_parts( $line ) {
+		$line = trim( $line );
+		$line = str_replace( "[hook ", "", $line );
+		$line = str_replace( "]", "", $line );
+		$parts = explode( " ", $line );
+		return $parts;
+	}
+
+	function get_hook( $parts ) {
+		return $parts[ 0 ];
+	}
+
+	function get_type( $parts ) {
+		return $parts[ 1 ];
+	}
+	
+	function get_num_args( $parts ) {
+		return $parts[ 2 ];
+	}
+	
+	function get_count( $parts ) {
+		return $parts[ 3 ];
+	}
+	
+	
+	function get_attached( $parts ) {
+		return $parts[ 4 ];
+	}
+		
+
+	function compare_matches( $gparts, $cparts ) {
+		if ( $this->get_hook( $gparts ) != $this->get_hook( $cparts ) ) {
+			die();
+		}
+		if ( $this->get_type( $gparts ) != $this->get_type( $cparts ) ) {
+			$this->oline( "Hook type changed!", $this->ghook );
+			
+			die();
+		}
+		
+		if ( $this->get_num_args( $gparts ) != $this->get_num_args( $cparts ) ) {
+			$this->oline( "Num args changed", $this->ghook );
+			die();
+		} 
+		
+		if ( $this->get_attached( $gparts ) != $this->get_attached( $cparts ) ) {
+			$this->oline( "Attached hooks changed", $this->ghook );
+			$this->changed++;
+		} elseif ( $this->get_count( $gparts ) != $this->get_count( $gparts ) ) {
+			$this->oline( "Invocations changed", $this->ghook );
+			$this->changed++;			
+		} else {
+		
+		
+      echo "Match," . $this->ghook;
+			$this->same++;
+		}
+	}
+
+	function deleted_classic( $cparts ) {
+	
+			echo "Deleted," . $this->chook;
+		$this->deleted++;			
+
+	}
+
+	function new_hook( $gparts ) {
+ 		echo "Added," . $this->ghook;
+		$this->added++;
+	}
+	
+}
--- opinions/class-oik-block-hook-checker.php
+++ opinions/class-oik-block-hook-checker.php
@@ -1,29 +1,146 @@
 <?php 
 
-
 /**
  * @copyright (C) Copyright Bobbing Wide 2018
  * @package oik-block
+ * 
  */
  
  
-
+/**
+ * Issue #25
+ 
+ * Related: https://github.com/WordPress/gutenberg/issues/1316
+ 
+Another way of checking plugin compatibility is to look internally into the code.
+The classic editor invokes hooks and filters in a certain way
+The block editor changes that way. 
+There's an issue to document the changes.
+It should be possible to build routines that determine if plugins attach functions to the hooks that are changing. 
+If the hook is no longer being invoked OR hooks are being invoked in a different sequence to expected there could be a problem. 
+As a test, let's see if we can detect an issue with hello-dolly, or any plugin that responds to the 'replace_editor' hook, or help hooks or screen option hooks.
+I'll raise a separate issue for this. 
+ */
  
 class oik_block_hook_checker {
 
 	public $opinions;				 
-
+	public $pre_requisites_satisfied = false;
+	public $hooks; 
+	 
 	function __construct() {
-	$this->opinions = array();
-	
+		$this->opinions = array();
+		$this->check_prerequisites();
 	}
 	
+	/**
+	 * Use the logic from oik-bwtrace to determine the attached hook functions
+	 * If any hooks that are attached are different from expected then we need to form an opinion
+	 * 
+	 */
 	function analyse() {
-		$this->opinions[] = new oik_block_editor_opinion( "A", false, "S", "I'm thinking about hooks", "TBC" );
+		if ( $this->pre_requisites_satisfied ) {
+			$this->get_hook_list();
+			$this->process_hook_list();
+		}
+			
+	}
+	
+	function check_prerequisites() {
+		if ( !function_exists( "bw_trace_get_attached_hooks" ) ) {
+			oik_require( "includes/bwtrace-actions.php", "oik-bwtrace" );
+		}
+		if ( !function_exists( "bw_trace_get_attached_hooks" ) ) {
+			$this->add_opinion( "A", "O", "Cannot perform hook analysis", "Install oik-bwtrace" );
+			$this->pre_requisites_satisfied = false;
+			
+		}	else {
+			$this->pre_requisites_satisfied = true;
+		}
+		return $this->pre_requisites_satisfied;
+
+
+	}
+	
+	function add_opinion( $edit, $mandatory, $observation, $notes=null ) {
+		$this->opinions[] = new oik_block_editor_opinion( $editor, $mandatory, "S", $observation, $notes );
+	}
+	
+	/**
+	 * Gets the list of hooks to check
+	 * 
+	 * We should also be able to determine the `expected` hook list and store this nicely
+	 
+    [hooks] => dbx_post_advanced,gutenberg_can_edit_post_type,replace_editor
+    [results] => the_content:9,the_content:10,the_content:11,the_content:12,the_content:21,the_content:99,the_content,_wp_relative_upload_path
+	 */
+	
+	function get_hook_list() {
+		$hooks = array();
+		$hooks[] = "admin_body_class";
+		$hooks[] = "admin_enqueue_scripts"; 
+		$hooks[] = "replace_editor";
+		$hooks[] = "dbx_post_advanced";
+		$hooks[] = 'the_content';
+		
+		$this->hooks = $hooks;
+	}
+	
+	function process_hook_list() {
+		foreach ( $this->hooks as $hook ) {
+			//echo $hook . PHP_EOL;
+			$this->get_attached_hooks( $hook );
+			//gob();
+		}
+	}
+	
+	/**
+	 * Gets the attached hooks
+	 *
+	 * @param string $hook
+	 * @return string attached hooks 
+	 */
+	function get_attached_hooks( $hook ) {
+		$attached = bw_trace_get_attached_hooks( $hook );
+		echo $hook;
+		echo "!";
+		echo $attached;
+		echo "!";
+		echo PHP_EOL;
+		return $attached;
 	}
 	
 	function get_opinions() {
+		if ( 0 === count( $this->opinions ) ) {
+			$this->opinions[] = new oik_block_editor_opinion( "A", false, "S", "No problems with hooks detected", "TBC" );
+		}
 		return $this->opinions;
 	}
+	
+	function replace_editor_expected_output() {
+		$expected = ": 10   gutenberg_init;2";
+		return $expected;
+	}
+	
+	/** 
+	 * The [hook] shortcode produced by oik-bwtrace
+	 * is formatted as 
+	 * 
+	 * `[hook $hook $type $num_args $count $attached]`
+	 * 
+	 * If, for the edit transaction ( e.g. /src/wp-admin/post.php?post=813&action=edit ) 
+	 *  we compare the hooks invoked by Gutenberg vs the hooks invoked when using the Classic editor 
+	 * ( e.g. /src/wp-admin/post.php?post=813&action=edit&classic-editor=1 )
+	 * we'll determine the changes
+	 * 
+	 * 
+	 * Hook             | Type   | Change         | Details                                                           |	Observation 
+	 * ---------------- |------- | -------------  | ----------------------------------------------------------------  | -------
+	 * admin_body_class | filter | attached count | Gutenberg adds "gutenberg-editor-page" (inside "replace_editor" ) | None
+	 * admin_enqueue_scripts | action | attached count | 
+	 */
+
+	
+	
 
 }
