664187bd7a6020fb4e3661859ed0e7ecd876b13a
Herb Miller <herb@bobbingwide.com>
2018-11-28 13:20:08 +0000

    Issue #30 - [guts] - display WordPress and Gutenberg version information

--- shortcodes/oik-guts.php
+++ shortcodes/oik-guts.php
@@ -1,14 +1,41 @@
 <?php // (C) Copyright Bobbing Wide 2018
 
+/**
+ * Implement [guts] shortcode
+ * 
+ * Displays information about WordPress and/or Gutenberg
+ * 
+ */
+
 function oik_block_guts( $atts, $content, $tag ) {
- oik_block_display_constant( "GUTENBERG_LOAD_VENDOR_SCRIPTS", "bool" );
- oik_block_display_constant( "GUTENBERG_LIST_VENDOR_ASSETS", "bool" );
- oik_block_display_constant( "GUTENBERG_DEVELOPMENT_MODE", "bool" );
+
+	p( bw_wp( array( "v" ) ) ); // [wp v]
+  // gutenberg version 
+	p( "Gutenberg version: "  . oik_block_gutenberg_version() );
+	
+	//oik_block_display_constant( "GUTENBERG_VERSION", "string" );
+	oik_block_display_constant( "GUTENBERG_LOAD_VENDOR_SCRIPTS", "bool" );
+	oik_block_display_constant( "GUTENBERG_LIST_VENDOR_ASSETS", "bool" );
+	oik_block_display_constant( "GUTENBERG_DEVELOPMENT_MODE", "bool" );
 
 
  return bw_ret();
 
 }
+
+/**
+ * Returns the Gutenberg version, if it's activated
+ * 
+ * @return string value of GUTENBERG_VERSION constant
+ */
+function oik_block_gutenberg_version() {
+	$version = null;
+	if ( defined( 'GUTENBERG_VERSION' ) ) {
+		$version = GUTENBERG_VERSION;
+	}
+	return $version;
+}  
+	
 
 
 
